c20c477c815d495f54e8aff11f589c8a7cd02b60
DJ <umdjm@hotmail.com>
2013-11-14 19:40:32 -0500

    fix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,9 @@ SemiDndFileManager::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.paperclip_defaults = {
-      :storage => :s3
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => 'mattsfilemanager'
+      }
   }
 end
